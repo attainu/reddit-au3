08690cdc00d8553812ccc7e9d60373b55d843431
DIVYANSHU TRIPATHI <dt565234@gmail.com>
2020-02-15 13:03:43 +0530

    change

--- server/index.js
+++ server/index.js
@@ -10,58 +10,74 @@ const app = express();
 const { User } = require('./database');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extends: false }));
 
-app.post('/signup', async(req, res) => {
-    let checkusername = await User.findAll({
-        where: {Username: {[Sequelize.Op.eq]: req.body.Username}}
-    });
+app.post('/signup', (req, res) => {
+  const errors = {};
+  const userData = new User({
+    Username: req.body.Username,
+    Password: req.body.Password
+  });
 
-    if(checkUsername.length === 0) {
-        try {
-            const hashPassword = await bcrypt.hash(req.body.Password, 10);
-            let user = {...req.body, Password: hashPassword};
-            let newUser = await User.create(user);
-            const token = jwt.sign(newUser.dataValues, process.env.SECRET_OR_KEY);
-            res.json({username: newUser.dataValues.Username, token: token});
-        }
-        catch {
-            return res.status(422).send();
-        }
+  User.findOne({
+    where: { Username: req.body.Username }
+  }).then(item => {
+    if (!item) {
+      bcrypt.genSalt(12, (err, salt) => {
+        if (err) throw err;
+        bcrypt.hash(userData.Password, salt, (err, hasted) => {
+          userData.Password = hasted;
+          userData
+            .save()
+            .then(user => {
+              if (!user) {
+                return res.status(400).json(errors);
+              } else {
+                res.status(200).json(user);
+              }
+            })
+            .catch(err => console.log(err));
+        });
+      });
+    } else {
+      res.json('User Already exist');
     }
-    else {
-        return res.status(409).send();
+  });
+
+
+});
+
+app.get('/allusers', (req, res) => {
+  User.findAll({})
+    .then(item => {
+      if (!item) {
+        res.json('no Users');
+      } else {
+        res.json(item);
+      }
+    })
+    .catch(err => console.log(err));
+});
+
+app.post('/signin', async (req, res) => {
+  let user = await User.findAll({
+    where: { Username: { [Sequelize.Op.eq]: req.body.Username } }
+  });
+
+  if (user.length === 0) {
+    return res.status(400).send();
+  }
+
+  try {
+    if (await bcrypt.compare(req.body.Password, user[0].dataValues.Password)) {
+      const payload = user[0].dataValues;
+      const token = jwt.sign(payload, process.env.SECRET_OR_KEY);
+      res.json({ username: payload.Username, token: token });
     }
-       
-})
-
-app.post('/signin', async(req, res) => {
-    let user = await User.findAll({
-        where: {Username: {[Sequelize.Op.eq]: req.body.Username}}
-    });
-
-    if(user.length === 0) {
-        return res.status(400).send();
-    }
-
-    try {
-        if(await bcrypt.compare(req.body.Password, user[0].dataValues.Password)) {
-            const payload = user[0].dataValues;
-            const token = jwt.sign(payload, process.env.SECRET_OR_KEY);
-            res.json({username: payload.Username, token: token});
-        }
-    }
-    catch {
-        res.status(500).send();
-    }
-})
-
-app.get('/userinfo/:username', async(req, res) => {
-    let userData = await User.findAll({
-        where: {Username: {[Sequelize.Op.eq]: req.params.Username}}
-    });
-
-    res.json(userData[0].dataValues);
-})
+  } catch {
+    res.status(500).send();
+  }
+});
 
 
 
