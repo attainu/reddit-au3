4d09650f072cf74ac8127a63bfb0470579f81ad9
DIVYANSHU TRIPATHI <dt565234@gmail.com>
2020-03-09 20:57:43 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 const config = require('./config');
+const path = require('path');
 
 const connect = url => {
   return mongoose.connect(url, config.db.options);
@@ -11,5 +12,14 @@ if (require.main === module) {
   connect(config.db.prod);
   mongoose.connection.on('error', console.log);
 }
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static( '../client/build' ));
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+  });
+}
+
 
 module.exports = { connect };
